7fe68d7c1d639a3491a39679611cda0126d0a7f1
acy29 <andres29111990@gmail.com>
2014-07-06 08:50:10 -0430

    Google maps parte 1

---
+++ GoogleMapsV1/gen/com/desarrollolibre/googlemapsv1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.desarrollolibre.googlemapsv1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ GoogleMapsV1/src/com/desarrollolibre/googlemapsv1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.desarrollolibre.googlemapsv1;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends android.support.v4.app.FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
